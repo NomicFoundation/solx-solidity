matter-labs/era-solidity

Implement changes in the codegen, standard-json interface etc. for the downstream toolchain
- Change the version dump.
- Disable EVM bytecode generation.
- Report recursive functions in a new standard-json output field called
  "extraMetadata" so that zksolc can lower them correctly.
- Disable the optimiser in the legacy pipeline.
- Generate a simpler, but equivalent, control flow in the try-catch lowering in
  the legacy pipeline to not confuse the zksolc translator.
- Generate a dispatch table in the lowering of indirect internal function call
  in the legacy pipeline.
- Define ZKEVM yul intrinsics.

libyul/AsmAnalysis.cpp
@@ -109,6 +109,18 @@ AsmAnalysisInfo AsmAnalyzer::analyzeStrictAssertCorrect(
 	return analysisInfo;
 }
 
+AsmAnalysisInfo AsmAnalyzer::analyzeStrictAssertCorrect(
+	Dialect const& _dialect, Object const& _object, yul::ExternalIdentifierAccess::Resolver _resolver)
+{
+	ErrorList errorList;
+	langutil::ErrorReporter errors(errorList);
+	AsmAnalysisInfo analysisInfo;
+	bool success = yul::AsmAnalyzer(analysisInfo, errors, _dialect, _resolver, _object.qualifiedDataNames())
+					   .analyze(_object.code()->root());
+	yulAssert(success && !errors.hasErrors(), "Invalid assembly/yul code.");
+	return analysisInfo;
+}
+
 size_t AsmAnalyzer::operator()(Literal const& _literal)
 {
 	bool erroneousLiteralValue = false;

test/solc/CommandLineInterface.cpp
@@ -141,7 +141,8 @@ BOOST_AUTO_TEST_CASE(version)
 	OptionsReaderAndMessages result = runCLI({"solc", "--version"}, "");
 
 	BOOST_TEST(result.success);
-	BOOST_TEST(boost::ends_with(result.stdoutContent, "Version: " + solidity::frontend::VersionString + "\n"));
+	BOOST_TEST(boost::ends_with(result.stdoutContent, "Version: " + solidity::frontend::VersionString + "\n"
+				+ "ZKsync: " + solidity::frontend::ZKsyncVersionString + "\n"));
 	BOOST_TEST(result.stderrContent == "");
 	BOOST_TEST(result.options.input.mode == InputMode::Version);
 }

NomicFoundation/solx-solidity

libyul/AsmAnalysis.cpp
@@ -155,6 +155,18 @@ AsmAnalysisInfo AsmAnalyzer::analyzeStrictAssertCorrect(
 	return analysisInfo;
 }
 
+AsmAnalysisInfo AsmAnalyzer::analyzeStrictAssertCorrect(
+	Dialect const& _dialect, Object const& _object, yul::ExternalIdentifierAccess::Resolver _resolver)
+{
+	ErrorList errorList;
+	langutil::ErrorReporter errors(errorList);
+	AsmAnalysisInfo analysisInfo;
+	bool success = yul::AsmAnalyzer(analysisInfo, errors, _dialect, _resolver, _object.summarizeStructure())
+					   .analyze(_object.code()->root());
+	yulAssert(success && !errors.hasErrors(), "Invalid assembly/yul code.");
+	return analysisInfo;
+}
+
 size_t AsmAnalyzer::operator()(Literal const& _literal)
 {
 	bool erroneousLiteralValue = false;

test/solc/CommandLineInterface.cpp
@@ -141,7 +141,8 @@ BOOST_AUTO_TEST_CASE(version)
 	OptionsReaderAndMessages result = runCLI({"solc", "--version"}, "");
 
 	BOOST_TEST(result.success);
-	BOOST_TEST(boost::ends_with(result.stdoutContent, "Version: " + solidity::frontend::VersionString + "\n"));
+	BOOST_TEST(boost::ends_with(result.stdoutContent, "Version: " + solidity::frontend::VersionString + "\n"
+				+ "LLVM: " + solidity::frontend::ZKsyncVersionString + "\n"));
 	BOOST_TEST(result.stderrContent == "");
 	BOOST_TEST(result.options.input.mode == InputMode::Version);
 }