2243156ebd88193bff483fa6b5495d474948a46f
Marco Castignoli <marco@argot.org>
2026-01-08 14:22:56 +0100

    enforce CCACHE_KEY and fix Windows ccache CMake globals

--- scripts/ci/build_win.sh
+++ scripts/ci/build_win.sh
@@ -6,6 +6,7 @@ prerelease_source="${1:-ci}"
 ROOTDIR="$(realpath "$(dirname "$0")/../../")"
 FORCE_RELEASE="${FORCE_RELEASE:-}"
 CIRCLE_TAG="${CIRCLE_TAG:-}"
+CMAKE_VS_GLOBALS_ARG=()
 
 cd "$ROOTDIR"
 "${ROOTDIR}/scripts/prerelease_suffix.sh" "$prerelease_source" "$CIRCLE_TAG" > prerelease.txt
@@ -24,7 +25,8 @@ if [[ "${CCACHE_ENABLED:-}" == "1" ]]; then
   WIN_PWD="$(pwd -W)"
   # Visual Studio generator doesn't use compiler launchers; use ccache masquerade.
   # See https://github.com/ccache/ccache/wiki/MS-Visual-Studio
-  CMAKE_OPTIONS="${CMAKE_OPTIONS:-} -DCMAKE_VS_GLOBALS=CLToolPath=${WIN_PWD}/deps/ccache;UseMultiToolTask=true"
+  CMAKE_VS_GLOBALS="CLToolPath=${WIN_PWD}/deps/ccache;UseMultiToolTask=true"
+  CMAKE_VS_GLOBALS_ARG=(-DCMAKE_VS_GLOBALS="$CMAKE_VS_GLOBALS")
   mkdir -p "$CCACHE_DIR"
 fi
 
@@ -40,6 +42,7 @@ boost_dir=("${ROOTDIR}/deps/boost/lib/cmake/Boost-"*)
     -DBoost_DIR="${boost_dir[*]}" \
     -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded \
     -DCMAKE_INSTALL_PREFIX="${ROOTDIR}/uploads/" \
+    "${CMAKE_VS_GLOBALS_ARG[@]}" \
     ${CMAKE_OPTIONS:-} \
     ..
 if [[ "${CCACHE_ENABLED:-}" == "1" ]]; then
