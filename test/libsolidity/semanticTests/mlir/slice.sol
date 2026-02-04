cd6b45d2b1154e5875d4d98fa660e683bea66c1e
Abinav Puthan Purayil <abinavpp@gmail.com>
2026-02-04 12:48:31 +0100

    [mlir] Add slice revert tests

--- test/libsolidity/semanticTests/mlir/slice.sol
+++ test/libsolidity/semanticTests/mlir/slice.sol
@@ -34,3 +34,5 @@ contract C {
 // sliceToEnd(uint256[],uint256): 0x40, 2, 4, 10, 20, 30, 40 -> 0x20, 2, 30, 40
 // sliceFull(uint256[]): 0x20, 4, 10, 20, 30, 40 -> 0x20, 4, 10, 20, 30, 40
 // sliceLen(uint256[],uint256,uint256): 0x60, 1, 3, 4, 10, 20, 30, 40 -> 2
+// slice(uint256[],uint256,uint256): 0x60, 3, 1, 4, 10, 20, 30, 40 -> FAILURE, hex"08c379a0", 0x20, 22, "Slice starts after end"
+// slice(uint256[],uint256,uint256): 0x60, 0, 5, 4, 10, 20, 30, 40 -> FAILURE, hex"08c379a0", 0x20, 28, "Slice is greater than length"
