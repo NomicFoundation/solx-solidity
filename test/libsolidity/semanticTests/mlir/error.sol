df674e47080eba977eaf5c8049fb476cb7129478
Abinav Puthan Purayil <abinavpp@gmail.com>
2026-02-04 14:10:41 +0100

    [mlir] D'oh! Test custom errors via mlir, not yul

--- test/libsolidity/semanticTests/mlir/error.sol
+++ test/libsolidity/semanticTests/mlir/error.sol
@@ -1,12 +1,17 @@
 contract C {
   error E(uint a);
+  error E2(uint a, uint b);
+  error Empty();
+
   function f(uint a) public { revert E(a); }
+  function g(uint a, uint b) public { revert E2(a, b); }
+  function h() public { revert Empty(); }
 }
 
-// TODO:
-// function g(uint a) public { require(a != 0, E(a)); }
-// g(uint256): 0 -> FAILURE, hex"002ff067", 0
 // ====
-// compileViaYul: true
+// compileViaMlir: true
 // ----
 // f(uint256): 1 -> FAILURE, hex"002ff067", 1
+// f(uint256): 42 -> FAILURE, hex"002ff067", 42
+// g(uint256,uint256): 1, 2 -> FAILURE, hex"058c8f48", 1, 2
+// h() -> FAILURE, hex"3db2a12a"
