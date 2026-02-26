d11c9858b2ac6cdc710e9589baf25d624b9e14d1
Vladimir Radosavljevic <vr@matterlabs.dev>
2026-02-26 16:58:07 +0100

    [mlir] Cast bytes before cmp and some bitwise operations
    sol.cmp expects integer operands, so if inputs are bytes,
    we need to cast them to integers before the comparison.
    
    Signed-off-by: Vladimir Radosavljevic <vr@matterlabs.dev>

--- test/libsolidity/semanticTests/mlir/bytes.sol
+++ test/libsolidity/semanticTests/mlir/bytes.sol
@@ -22,6 +22,58 @@ contract C {
   function l(bytes memory a) public returns (uint) {
     return a.length;
   }
+
+  function eq3232(bytes32 a, bytes32 b) public returns (bool) {
+    return a == b;
+  }
+
+  function eq45(bytes4 a, bytes5 b) public returns (bool) {
+    return a == b;
+  }
+
+  function eq(bytes4 a, bytes4 b) public returns (bool) {
+    return a == b;
+  }
+
+  function ne(bytes4 a, bytes4 b) public returns (bool) {
+    return a != b;
+  }
+
+  function lt(bytes2 a, bytes2 b) public returns (bool) {
+    return a < b;
+  }
+
+  function lte(bytes2 a, bytes2 b) public returns (bool) {
+    return a <= b;
+  }
+
+  function gt(bytes2 a, bytes2 b) public returns (bool) {
+    return a > b;
+  }
+
+  function gte(bytes2 a, bytes2 b) public returns (bool) {
+    return a >= b;
+  }
+
+  function b45(bytes4 a) public returns (bytes5) {
+    return bytes5(a);
+  }
+
+  function b54(bytes5 a) public returns (bytes4) {
+    return bytes4(a);
+  }
+
+  function and45(bytes4 a, bytes5 b) public returns (bytes5) {
+    return a & b;
+  }
+
+  function or45(bytes4 a, bytes5 b) public returns (bytes5) {
+    return a | b;
+  }
+
+  function xor45(bytes4 a, bytes5 b) public returns (bytes5) {
+    return a ^ b;
+  }
 }
 
 // ====
@@ -34,3 +86,18 @@ contract C {
 // ld(bytes): 32, 5, "hello" -> 0x6800000000000000000000000000000000000000000000000000000000000000
 // st(bytes): 32, 5, "hello" -> 32, 5, 0x61656c6c6f000000000000000000000000000000000000000000000000000000
 // l(bytes): 32, 5, "hello" -> 5
+// b45(bytes4): left(0x01020304) -> left(0x0102030400)
+// b54(bytes5): left(0x0102030405) -> left(0x01020304)
+// and45(bytes4,bytes5): left(0x12345678), left(0xAAAABBBBCC) -> left(0x0220123800)
+// or45(bytes4,bytes5): left(0x12345678), left(0xAAAABBBBCC) -> left(0xBABEFFFBCC)
+// xor45(bytes4,bytes5): left(0x12345678), left(0xAAAABBBBCC) -> left(0xB89EEDC3CC)
+// eq3232(bytes32,bytes32): "abcdefghijklmnopqrstuvwxyzabcdef", "abcdefghijklmnopqrstuvwxyzabcdef" -> true
+// eq45(bytes4,bytes5): left(0x01020304), left(0x0102030400) -> true
+// eq45(bytes4,bytes5): left(0x01020304), left(0x0102030405) -> false
+// eq(bytes4,bytes4): left(0x01020304), left(0x01020304) -> true
+// eq(bytes4,bytes4): left(0x01020304), left(0x01020305) -> false
+// ne(bytes4,bytes4): left(0x01020304), left(0x01020305) -> true
+// lt(bytes2,bytes2): "aa", "ab" -> true
+// lte(bytes2,bytes2): "ab", "ab" -> true
+// gt(bytes2,bytes2): "ab", "aa" -> true
+// gte(bytes2,bytes2): "ab", "ab" -> true
