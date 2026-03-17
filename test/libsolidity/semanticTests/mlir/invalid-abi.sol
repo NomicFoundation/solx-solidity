8413b91ff64766f3cf2f57bf402b034cb51d6407
Vladimir Radosavljevic <vr@matterlabs.dev>
2026-03-17 12:44:20 +0100

    [mlir] Add tests for Add length guard for dynamic-memory allocation during ABI decoding
    Signed-off-by: Vladimir Radosavljevic <vr@matterlabs.dev>

--- test/libsolidity/semanticTests/mlir/invalid-abi.sol
+++ test/libsolidity/semanticTests/mlir/invalid-abi.sol
@@ -23,4 +23,5 @@ contract C {
 // g(string): 0x10000000000000000 -> FAILURE, hex"08c379a0", 0x20, 0x22, "ABI decoding: invalid tuple offs", "et"
 // h(uint256[][]): 32, 2, 0x200, 0xa0, 2, 1, 2, 2, 9, 8 -> FAILURE, hex"08c379a0", 0x20, 0x2b, "ABI decoding: invalid calldata a", "rray offset"
 // h(uint256[][]): 32, 1, 0x10000000000000000 -> FAILURE, hex"08c379a0", 0x20, 0x2b, "ABI decoding: invalid calldata a", "rray offset"
+// h(uint256[][]): 32, 1, 32, 3618502788666131106986593281521497120414687020801267626233049500247285301248 -> FAILURE, hex"4e487b71", 0x41
 // e() -> FAILURE, hex"08c379a0", 0x20, 0x2b, "ABI decoding: invalid calldata a", "rray stride"
